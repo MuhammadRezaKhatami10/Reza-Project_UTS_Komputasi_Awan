1bd7fcb94ab7c8487f876260f9b31513fe89c6a6
MuhRezaK <90266254+MuhammadRezaKhatami10@users.noreply.github.com>
2022-10-28 09:23:12 +0700

    Add files via upload

---
+++ page/tentang.php
@@ -0,0 +1,49 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Reza | Dyko</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: DIV Teknik Informatika</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: Ngoding, Belajar bahasa pemrograman baru, Naik Gunung, Futsal, Berenang, dll</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Minat</b></li>
+        </td>
+        <td>: I'M Web Developer</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
